9dae592dde23e4c567e9313f52e734e35a24108d
Glenn Sonna <kenpachi.os@gmail.com>
2015-03-02 17:01:59 +0100

    added constants

---
+++ app/src/main/java/com/arduino/blue/theglenn/arduinoblue/Constants.java
@@ -0,0 +1,12 @@
+package com.arduino.blue.theglenn.arduinoblue;
+
+/**
+ * Created by theGlenn on 02/03/15.
+ */
+public class Constants {
+    public static final String DEVICE_KEY = "btdevice";
+    public static final String BLE_TAG = "BLE";
+
+    public static boolean FILTER = false;
+    public static final String FILTER_TAG = "RNBT";
+}
